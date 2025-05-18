0f71cc3583792c6a7045cb10e881e216ef3f80ca
AeroXuk <AeroXuk@users.noreply.github.com>
2025-05-18 07:06:00 +0000

    Fix error

--- components/cc1101/__init__.py
+++ components/cc1101/__init__.py
@@ -2,7 +2,7 @@ from esphome import automation, pins
 from esphome.components import spi
 import esphome.codegen as cg
 import esphome.config_validation as cv
-import esphome.const import (
+from esphome.const import (
     CONF_ID,
     CONF_CLK_PIN,
     CONF_CS_PIN,
